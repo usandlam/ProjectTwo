8aafdd9eea914aad133ecfebeefccd322954e3b3
Andrew Lamagna <andrew.lamagna@LEGO.com>
2022-04-29 15:36:48 -0400

    More UI improvements

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -5,6 +5,7 @@ const control = require('../middleware/mustBeLoggedIn.js');
 // const control = require('../middleware/sessionTrack.js');
 const userTrack = require('../middleware/userTrack.js')
 
+const User = require('../models/User.model');
 
 /* GET home page */
 router.get("/", (req, res, next) => {
@@ -21,8 +22,15 @@ router.get("/main2", control(), (req, res, next) => {
   res.render("main",{userSession: req.session.passport});
 });
 
-router.get("/profile", control(), (req, res, next) => {
-  res.render("profile",);
+router.get("/profile", control(), async (req, res, next) => {
+  const id = req.session.passport.user;
+  try{
+    const userDetails = await User.findById(id);
+    // console.log(userDetails);
+    res.render("profile",{user: userDetails});
+  }catch (err){
+    console.log(err);
+  }
 });
 
 
